40d0d73cb109c7c30d637b2a7d0a8dd62c4353dc
Arata Tanaka <tarata43@yahoo.co.jp>
2016-08-22 20:33:57 +0900

    Add group

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ func main() {
 	r := &repositories.TodoRepositoryOnMemory{Data: map[int]*models.Todo{}}
 	t := &controllers.Todo{Repository: r}
 	router := gin.Default()
-	router.GET("/todo", t.ShowAll)
-	router.GET("/todo/:id", t.Show)
-	router.POST("/todo", t.Create)
-	router.PUT("/todo/:id", t.Update)
-	router.DELETE("/todo/:id", t.Delete)
+	api := router.Group("/api")
+	{
+		api.GET("/todo", t.ShowAll)
+		api.GET("/todo/:id", t.Show)
+		api.POST("/todo", t.Create)
+		api.PUT("/todo/:id", t.Update)
+		api.DELETE("/todo/:id", t.Delete)
+	}
 	router.Run()
 }
